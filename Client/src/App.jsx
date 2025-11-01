3988a30e26399160b9c3c7e1882f871e398922ea
mohitvashu1 <mohitchoubeybca@gmail.com>
2025-11-01 20:11:53 +0530

    Routing & added Background

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,13 +1,22 @@
 
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import ProfilePage from './pages/ProfilePage'
 
 function App() {
 
   return (
-    <div>
-      <h1 className='text-red-800'>Mohit Choubey</h1>
+    <div className='bg-[url("./src/assets/bgImage.svg")] bg-contain '>
+    <Routes>
+      <Route path='/' element={<HomePage/>}/>
+      <Route path='/login' element={<LoginPage/>}/>
+      <Route path='/profile' element={<ProfilePage/>}/>
+    </Routes>
     </div>
   )
 }
 
 export default App
+
